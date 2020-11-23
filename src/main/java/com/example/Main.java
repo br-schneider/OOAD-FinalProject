efe762780492fdb96dfb28ff74b8d9c1e3bb4956
Brett Schneider <brett@Bretts-MBP.home>
2020-11-23 13:19:05 -0800

    Test
    11/23

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -38,7 +22,6 @@ import java.util.List;
 @Controller
 @SpringBootApplication
 public class Main {
-
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
@@ -56,7 +39,6 @@ public class Main {
   String index() {
     return "index";
   }
-
 
 
 //  @RequestMapping("/db")
@@ -120,18 +102,11 @@ public class Main {
   }
 
 
-  @RequestMapping("/car")
-  String car(Map<String, Object> model) {
+  @RequestMapping("/car_inventory")
+  String car_inventory(Map<String, Object> model) {
     List<Car> dbCarList = getCarsFromDB();
 
     ArrayList<String> output = new ArrayList<String>();
-
-//    for (int i = 0; i < dbCarList.size(); i++) {
-//      output.add(dbCarList.get(i).printCar());
-//
-//
-//    }
-
 
     model.put("car1Title", dbCarList.get(0).printMakeModelYear());
     model.put("car1Description", dbCarList.get(0).quickDescription());
@@ -144,7 +119,7 @@ public class Main {
     model.put("car3Title", dbCarList.get(2).printMakeModelYear());
     model.put("car3Description", dbCarList.get(2).quickDescription());
     model.put("car3Price", dbCarList.get(2).printPricing());
-    return "car";
+    return "car_inventory";
   }
 
   @RequestMapping("/car_profile")
