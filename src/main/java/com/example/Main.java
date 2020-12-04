1fcfc1d9fad96a01ec7c006689934984ebf25ff0
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2020-12-04 11:18:22 -0600

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -227,6 +227,55 @@ public class Main {
     }
   }
 
+  List<Coupe> getCoupesFromDB() {
+    List<Coupe> carsList = new ArrayList<Coupe>();
+
+    try (Connection connection = dataSource.getConnection()) {
+      String query = "SELECT * FROM car_inventory WHERE car_type like '%Coupe%'";
+      Statement stmt = connection.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+      String make, model2, color;
+      int year, mileage;
+      double price;
+
+      while (rs.next()) {
+        make = rs.getString("make");
+        model2 = rs.getString("model");
+        year = rs.getInt("year");
+        mileage = rs.getInt("mileage");
+        color = rs.getString("color");
+        price = rs.getDouble("price");
+
+        Coupe testCar = new Coupe();
+        testCar.setMake(make);
+        testCar.setModel(model2);
+        testCar.setYear(year);
+        testCar.setMileage(mileage);
+        testCar.setColor(color);
+        testCar.setPrice(price);
+
+        carsList.add(testCar);
+      }
+
+      return carsList;
+
+    } catch (Exception e) {
+      Coupe errorCar = new Coupe();
+
+      errorCar.setMake("-1");
+      errorCar.setModel("-1");
+      errorCar.setYear(-1);
+      errorCar.setMileage(-1);
+      errorCar.setColor("-1");
+      errorCar.setPrice(-1);
+
+      carsList.add(errorCar);
+
+      return carsList;
+    }
+  }
+
 
   @RequestMapping("/search")
   public String search(@RequestParam(defaultValue="errorSearch") String FULL_SEARCH, Model model) {
@@ -255,7 +304,7 @@ public class Main {
         output.add(
           "                <div class=\"d-flex w-100 justify-content-between\">\n" +
           "                    <h5 class=\"mb-1\" >" + model2 + "</h5>\n" +
-          "                    <small>" + days + "days ago </small>\n" +
+          "                    <small>" + days + " days ago </small>\n" +
           "                </div>\n" +
           "                <p class=\"mb-1\"> " + make + "</p>\n" +
           "                <small blah\">blahblah</small>\n"
@@ -286,7 +335,7 @@ public class Main {
         output.add(
                 "                <div class=\"d-flex w-100 justify-content-between\">\n" +
                         "                    <h5 class=\"mb-1\" >" + model2 + "</h5>\n" +
-                        "                    <small>" + days + "  -days ago </small>\n" +
+                        "                    <small>" + days + " days ago </small>\n" +
                         "                </div>\n" +
                         "                <p class=\"mb-1\"> " + make + "</p>\n" +
                         "                <small blah\">blahblah</small>\n"
@@ -316,7 +365,37 @@ public class Main {
         output.add(
                 "                <div class=\"d-flex w-100 justify-content-between\">\n" +
                         "                    <h5 class=\"mb-1\" >" + model2 + "</h5>\n" +
-                        "                    <small>" + days + "  -days ago </small>\n" +
+                        "                    <small>" + days + " days ago </small>\n" +
+                        "                </div>\n" +
+                        "                <p class=\"mb-1\"> " + make + "</p>\n" +
+                        "                <small blah\">blahblah</small>\n"
+        );
+      }
+
+      model.addAttribute("messages", output);
+
+      return "search";
+    }
+    else if (values[0].equals("Coupe") ) {
+
+      List<Coupe> dbCarList = getCoupesFromDB();
+      String model2;
+      String make;
+
+      amountOfResults = dbCarList.size();
+
+      ArrayList<String> output = new ArrayList<String>();
+
+      for (int i = 0; i < amountOfResults; i++) {
+        double doubledays = Math.round((Math.random() * (30 - 1)) + 1);
+        int days = (int) doubledays;
+        model2 = dbCarList.get(i).getModel();
+        make = dbCarList.get(i).getMake();
+
+        output.add(
+                "                <div class=\"d-flex w-100 justify-content-between\">\n" +
+                        "                    <h5 class=\"mb-1\" >" + model2 + "</h5>\n" +
+                        "                    <small>" + days + " days ago </small>\n" +
                         "                </div>\n" +
                         "                <p class=\"mb-1\"> " + make + "</p>\n" +
                         "                <small blah\">blahblah</small>\n"
