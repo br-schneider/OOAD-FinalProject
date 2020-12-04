a28871f55c88bbcbd0a7e1e1fd7b7267eda31f61
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2020-12-04 10:51:15 -0600

    test

--- src/main/java/com/example/Truck.java
+++ src/main/java/com/example/Truck.java
@@ -1,6 +1,32 @@
 package com.example;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+
+
+
+
 public class Truck implements Car{
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Autowired
+    private DataSource dataSource;
+
 	
 	String make;
 	String model;
@@ -24,7 +50,7 @@ public class Truck implements Car{
     }
  
     public String getMake() {
-        return "The make chosen is "+make+".";
+        return make;
     }
     
     public void setModel(String model) {
@@ -32,7 +58,7 @@ public class Truck implements Car{
     }
     
     public String getModel() {
-    	return "The model chose is "+model+".";
+    	return model;
     }
 
     public void setYear(int year) {
@@ -47,9 +73,7 @@ public class Truck implements Car{
     	this.color = color;
     }
     
-    public String getColor() {
-    	return "The car color chosen is "+color+".";
-    }
+    public String getColor() { return color; }
     
     public void setMileage(int mileage) {
     	this.mileage = mileage;
@@ -65,6 +89,67 @@ public class Truck implements Car{
     
     public String getPrice() {
     	return "The price for the car chosen is "+price+".";
+    }
+
+
+    public List<Truck> getTrucksFromDB() {
+        List<Truck> carsList = new ArrayList<Truck>();
+
+        try (Connection connection = dataSource.getConnection()) {
+            String query = "SELECT * FROM car_inventory WHERE car_type like '%Truck%'";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            String make, model2, color;
+            int year, mileage;
+            double price;
+
+            while (rs.next()) {
+                make = rs.getString("make");
+                model2 = rs.getString("model");
+                year = rs.getInt("year");
+                mileage = rs.getInt("mileage");
+                color = rs.getString("color");
+                price = rs.getDouble("price");
+
+                Truck testCar = new Truck();
+                testCar.setMake(make);
+                testCar.setModel(model2);
+                testCar.setYear(year);
+                testCar.setMileage(mileage);
+                testCar.setColor(color);
+                testCar.setPrice(price);
+
+                carsList.add(testCar);
+            }
+
+            return carsList;
+
+        } catch (Exception e) {
+            Truck errorCar = new Truck();
+
+            errorCar.setMake("-1");
+            errorCar.setModel("-1");
+            errorCar.setYear(-1);
+            errorCar.setMileage(-1);
+            errorCar.setColor("-1");
+            errorCar.setPrice(-1);
+
+            carsList.add(errorCar);
+
+            return carsList;
+        }
+    }
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
     
 }
