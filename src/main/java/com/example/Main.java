91dde1342d6b2d3d1e4b01d52c4e23ca010013de
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2020-12-03 21:04:24 -0600

    new test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -157,7 +157,7 @@ public class Main {
         model2 = dbCarList.get(i).getModel();
         make = dbCarList.get(i).getMake();
 
-        model.put("message123",
+        output.add(
                 "       <div class=\"list-group\">\n" +
                         "            <a href=\"#\" class=\"list-group-item list-group-item-action flex-column align-items-start\">\n" +
                         "                <div class=\"d-flex w-100 justify-content-between\">\n" +
@@ -172,6 +172,7 @@ public class Main {
 
       }
 
+      model.put("message123", output);
 
 
 
