d2b347d6ca985c5472c92a1d4ef98aa3b4fd7839
Brett Schneider <brett@Bretts-MacBook-Pro.local>
2020-12-05 16:01:18 -0600

    Test

--- src/main/java/com/example/Salesperson.java
+++ src/main/java/com/example/Salesperson.java
@@ -1,19 +1,17 @@
 package com.example;
-
 import java.util.Observable;
 import java.util.Observer;
+import java.io.*;
 
-public class Salesperson implements Observer{
+
+public class Salesperson implements Observer {
 	private Store salesperson;
 
 	@Override
 	public void update(Observable observable, Object arg) {
-		System.out.println("test");
-
 		if (observable instanceof Store) {
-			salesperson =  (Store) observable;
-			System.out.println("test");
+			salesperson = (Store) observable;
+			System.out.println("Hello this is David, the salesman, thank you for your purchase!");
 		}
 	}
 }
-		
--- src/main/java/com/example/Store.java
+++ src/main/java/com/example/Store.java
@@ -1,8 +1,9 @@
 package com.example;
+
 import java.util.Observable;
+import java.io.*;
 
-
-public class Store extends Observable{
+public class Store extends Observable {
 	private String type = "store1";
 	private String name = "name";
 	private int sedanCount = 10;
@@ -11,12 +12,12 @@ public class Store extends Observable{
 	private int sportscarCount = 10;
 	private int truckCount = 10;
 	private int suvCount = 10;
-	
-public Store(String name) {
+
+	public Store(String name) {
 		this.name = name;
 	}
-	
-	public Store(int sedanCount,int coupeCount, int hatchbackCount, int sportscarCount, int truckCount, int suvCount) {
+
+	public Store(int sedanCount, int coupeCount, int hatchbackCount, int sportscarCount, int truckCount, int suvCount) {
 		this.sedanCount = sedanCount;
 		this.coupeCount = coupeCount;
 		this.hatchbackCount = hatchbackCount;
@@ -24,70 +25,76 @@ public Store(String name) {
 		this.truckCount = truckCount;
 		this.suvCount = suvCount;
 	}
-	
-	public void decrement(String carType) {
-		
-		if(carType.equalsIgnoreCase("sedan")) {
-			for (int i = 0; i < sedanCount; i++) {
-				sedanCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+
+	public void decrement(String carType) throws IOException {
+		FileWriter writer = new FileWriter("store_inventory.txt", true);
+
+		if (carType.equalsIgnoreCase("sedan")) {
 			if (sedanCount == 0) {
-				System.out.println("We are out of sedans sorry");
+				System.out.println("We are out of sedans sorry!");
+				return;
 			}
-		}
-		else if (carType.equalsIgnoreCase("coupe")) {
-			for (int i = 0; i < coupeCount; i++) {
-				coupeCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+			sedanCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + sedanCount + " sedans left");
+			writer.write("\r\n");
+
+		} else if (carType.equalsIgnoreCase("coupe")) {
 			if (coupeCount == 0) {
-				System.out.println("We are out of coupes sorry");
+				System.out.println("We are out of coupes sorry!");
+				return;
 			}
-		}
-		else if (carType.equalsIgnoreCase("truck")) {
-			for (int i = 0; i < truckCount; i++) {
-				truckCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+			coupeCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + coupeCount + " coupes left");
+			writer.write("\r\n");
+
+		} else if (carType.equalsIgnoreCase("truck")) {
 			if (truckCount == 0) {
-				System.out.println("We are out of trucks sorry");
+				System.out.println("We are out of trucks sorry!");
+				return;
 			}
-		}
-		else if (carType.equalsIgnoreCase("sports car")) {
-			for (int i = 0; i < sportscarCount; i++) {
-				sportscarCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+			truckCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + truckCount + " trucks left");
+			writer.write("\r\n");
+
+		} else if (carType.equalsIgnoreCase("sportscar")) {
 			if (sportscarCount == 0) {
-				System.out.println("We are out of sports cars sorry");
+				System.out.println("We are out of sports cars sorry!");
+				return;
 			}
-		}
-		else if (carType.equalsIgnoreCase("hatchback")) {
-			for (int i = 0; i < hatchbackCount; i++) {
-				hatchbackCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+			sportscarCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + sportscarCount + " sports cars left");
+			writer.write("\r\n");
+
+		} else if (carType.equalsIgnoreCase("hatchback")) {
 			if (hatchbackCount == 0) {
-				System.out.println("We are out of hatcbacks sorry");
+				System.out.println("We are out of hatcbacks sorry!");
+				return;
 			}
-		}
-		else if (carType.equalsIgnoreCase("SUV")) {
-			for (int i = 0; i < suvCount; i++) {
-				suvCount --;
-				this.setChanged();
-		        this.notifyObservers();
-			}
+			hatchbackCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + hatchbackCount + " hatchbacks left");
+			writer.write("\r\n");
+
+		} else if (carType.equalsIgnoreCase("SUV")) {
 			if (suvCount == 0) {
-				System.out.println("We are out of SUVs sorry");
+				System.out.println("We are out of SUVs sorry!");
+				return;
 			}
+			suvCount--;
+			this.setChanged();
+			this.notifyObservers();
+			writer.write("We have " + suvCount + " SUVs left");
+			writer.write("\r\n");
 		}
-		
+		writer.close();
 	}
-	
 }
